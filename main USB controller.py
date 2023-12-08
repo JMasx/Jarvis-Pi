5da5871f0566f2f2656b4982ba2a9da7804e585a
Jaden Mascarenhas <111895086+JMasx@users.noreply.github.com>
2023-12-08 16:55:47 -0700

    Add files via upload

--- main USB controller.py
+++ main USB controller.py
@@ -1,77 +1,74 @@
-# import the opencv module
+#in-system
 import os
-import socket
-import subprocess
+#import subprocess
 import time
 
+#external library
 import cv2
 import numpy
 
-capture = cv2.VideoCapture(0)
-cv2.namedWindow = ('HELLO THERE!')
-cnt=1
-prevPicTime=0
-audio = 'C:\\Users\\masca\\Downloads\\Jarvis.mp3'
-firstDetection=True
+#unchanging variables
+firstDetection=True                                                                                 #boolean flag for first detection of face
+cnt=1                                                                                               #initialization for photo count
+prevPicTime=0                                                                                       #initialization for time register
+face = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')         #uses pre-saved directory 'data.haarcascades' and a pre-trained model 'haarcascades...xml' to comprehend a typical face structure
 
 
-def bluetoothSeek():
-    addresses = []
-    devices = subprocess.run(['hcitool', 'scan'], Scan=True, Names=True)        #resources using HCITool
-    if not devices.returncode:                                                  #IF code does not give error:
-        print('Local devices are:\n ')
-        for i in devices.stdout.strip().split('\n'):
-            addresses.append(i.split([0]))
-        
-        for i in range(len(addresses)):
-            print(f'\t {i+1} {addresses[i]}')
+#changeable variables
+camera = cv2.VideoCapture(0)                                                                        #initialize camera capture to first USB slot
+cv2.namedWindow = ('HELLO THERE!')                                                                  #Arbitrary name for camera's window
+audio = 'Jarvis.mp3'                                                                                #audio file within local directory, can use full directory path if needed
+speakerAddress = "08:EB:ED:DA:7E:5C"                                                                #BlueTooth device address found from other code
+photoInterval = 10                                                                                  #changeable interval to take next photo
+windowTitle = 'Hello sir!'                                                                          #changeable name for the window that shows camera video feed
+#windowsSoundApp = 'Music.UI.exe'
+piSoundApp = 'lxmusic'
 
-    else:
-        print('No devices found.')
 
-def bluetoothConnect(addresses):
-    connection = subprocess.run('rfcomm', 'connect', '0', addresses )                        #rfcomm documentation as source for virtual serial port
-    if not connection.returncode:
-        print('Connected successfully')
-
-def bluetoothDisconnect():
-    disconnection=subprocess.run('rfcomm', 'release', '0')
-    if not disconnection.returncode:
-        print("Disconnected successfully")
-
-def playSound(audioFile):
+def playSound(audioFile):                                                                           #function that plays audio files
     try:
-        # Use start to open the default media player for the MP3 file (Windows command)
-        #os.system("start {}".format(audioFile))
-        subprocess.Popen(['start', 'Groove Music.exe', audioFile], shell=True, creationflags=subprocess.DETACHED_PROCESS)
-    except Exception as e:
-        print('Could not play audio: {%s}' % e)
+        os.system('xdg-open {}'.format(audioFile))                                                   #Raspbian/Linux using default app for sound
+        #subprocess.run([piSoundApp, audio])                                                        #Alternative: Specific app for Raspbian/Linux using terminal
 
-if bluetoothSeek:
-    inputIndex = input("Choose a device # from the list: ")
-    bluetoothConnect[bluetoothSeek[inputIndex-1]]
-while capture.isOpened():
-    # to read frame by frame
-    _, currentFrame = capture.read()
-    _, nextFrame = capture.read()
+        #os.system("start {}".format(audioFile))                                                    #Windows variant for playing audio file using default app
+        #subprocess.run(['start', windowsSoundApp, audioFile])                                      #Alternative: Specific app using terminal command
 
+    except Exception as e:                                                                          #necessary for a central system command to run external file
+        print('Incorrect formatting for audioFile: {%s}' % e)
+
+
+
+
+
+
+
+while camera.isOpened():
+    _, currentFrame = camera.read()                                                                 #initialize the current frame through the camera's read() function
+    _, nextFrame = camera.read()                                                                    #initialize the next frame through the camera's read() function
+    motionSens=numpy.sum(cv2.absdiff(currentFrame, nextFrame) > 60)                                 #checks these many frames per second for difference in image's matrices
+
+
+    if numpy.all(motionSens > 500):                                                                 #IF more than 500 pixels in the matric of the image change
+        picTime=time.time()                                                                         #Find current time
+        faceDetect = face.detectMultiScale(currentFrame)                                            #Attempt to detect a face in this frame
+        if len(faceDetect) != 0:                                                                    #IF there was a face detected
+            if firstDetection:                                                                      #IF the firstDetection flag is true
+                    playSound(audio)                                                                #plays audio using written method
+                    firstDetection=False                                                            #turn off first detection flag so audio does not play again
+            if picTime - prevPicTime > photoInterval:                                               #IF the last photo was taken more than the assigned interval ago (10 sec in this case)
+                frame = 'Face Found{%d}.png'%cnt                                                    #saving the same string as a filename for saving image
+                print('Face Found{%d}.png was captured' % cnt)                                      #possible error, replace with 'Movement was Found{}.png'.format(cnt)
+                cnt+=1                                                                              #increment everytime this loops (each photo taken)
+                prevPicTime = time.time()                                                           #Save time of photograph taken
+                cv2.imwrite(frame, currentFrame)                                                    #physically create the .jpg of the file with the same name and save to current directory
+    nextFrame = currentFrame                                                                        #typical video structure of changing frames per second, assigning next image frame to current constantly
+    cv2.imshow(windowTitle, currentFrame)                                                           #display the frames in a window with the given title
     
-    motionSens=numpy.sum(cv2.absdiff(currentFrame, nextFrame) > 60)    #checks these many frames per second for difference in image's matrices
-
-    if numpy.all(motionSens > 500):
-        picTime=time.time()
-        if picTime - prevPicTime > 10:
-            if firstDetection:
-                playSound(audio)
-                firstDetection=False
-            img_name = 'Movement was Found{%d}.png' % cnt                       #possible error, replace with 'Movement was Found{}.png'.format(cnt)
-            cnt+=1
-            prevPicTime = time.time()
-            cv2.imwrite(img_name, currentFrame)
-            print(img_name + ' was captured')
-    nextFrame = currentFrame
-            
-    cv2.imshow("Hello Sir", currentFrame)
-    if cv2.waitKey(1)%256 == 27:                                    #when press Esc key
-        capture.release()
-        exit()
+    
+    
+    #Closing the active code.
+    if cv2.waitKey(1)%256 == 27:                                                                    #when press Esc key
+        camera.release()                                                                            #release access to the USB camera
+        os.system('taskkill /f /im {}'.format('Music.UI.exe'))                                      #closes music app default for windows
+        os.system('pkill{}'.format('lxmusic'))
+        exit()                                                                                      #end the execution of code
